8cb3149ab7705c1ea5a194e5403160007a47dce4
heitor-jsr <heitorscalon@gmail.com>
2023-06-08 19:01:16 -0300

    refactor: move SUT creatin to a factory method

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,16 +1,30 @@
 import HttpClientSpy from "../../test/mock-http-client";
 import RemoteAuthentication from "./remote-authentication";
 
+type SutTypes = {
+  sut: RemoteAuthentication,
+  httpPostClientSpy: HttpClientSpy
+}
+
+const makeSut = (url: string = 'url'): SutTypes => {
+  const httpPostClientSpy = new HttpClientSpy();
+
+  // system under test - objeto que está sendo testado no momento.
+  const sut = new RemoteAuthentication(url, httpPostClientSpy);
+
+  return {
+    sut, 
+    httpPostClientSpy
+  }
+}
+
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    const url = 'url';
-    const httpPostClient = new HttpClientSpy();
+    const url = 'url2'
 
-    // system under test - objeto que está sendo testado no momento.
-    const sut = new RemoteAuthentication(url, httpPostClient);
-
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth();
 
-    expect(httpPostClient.url).toBe(url);
+    expect(httpPostClientSpy.url).toBe(url);
   })
 })
