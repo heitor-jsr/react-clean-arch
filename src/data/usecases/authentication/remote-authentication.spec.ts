e4df39efe16929032a3325b2fa464fdb00f22eff
heitor-jsr <heitorscalon@gmail.com>
2023-06-28 20:26:16 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,11 @@
 import { faker } from '@faker-js/faker'
 import { RemoteAuthentication } from './remote-authentication'
-import { HttpClientSpy } from '@/data/test/mock-http-client'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { AccountModel } from '@/domain/models/account-models'
-import { mockAccountModel, mockAuthentication } from '@/domain/test/mock-account'
+import { HttpClientSpy } from '@/data/test'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { AuthenticationParams } from '@/domain/usecases'
+import { AccountModel } from '@/domain/models'
+import { mockAccountModel, mockAuthentication } from '@/domain/test'
 
 type SutTypes = {
   sut: RemoteAuthentication
--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
-import { AccountModel } from '../models/account-models'
-import { AuthenticationParams } from '../usecases/authentication'
+import { AccountModel } from '../models'
+import { AuthenticationParams } from '@/domain/usecases'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
