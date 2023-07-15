0f18a1fc589d625e1984ba19d46c46bc70d2b4d8
heitor-jsr <heitorscalon@gmail.com>
2023-07-15 14:18:04 -0300

    test: ensure login will not load spinner and error on start

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,8 +2,14 @@ import React from "react"
 import { render } from '@testing-library/react'
 import Login from "./Login"
 
+// o teste deve ser independente dos hooks do react. por isso, deve-se evitar ao max
+// importa-los e usar eles dentro dos arquivos de teste. isso garante que seu teste
+// passe, independente do que você estiver utilizando para renderizar os seus componentes.
+
 describe('Login component', () => {
-  test('Login', () => {
-    render(<Login />)
+  test('Should mock render spinner and error on start', () => {
+    const { getByTestId } = render(<Login />)
+    const errorWrap = getByTestId('error-wrap')
+    expect(errorWrap.childElementCount).toBe(0)
   })
 })
