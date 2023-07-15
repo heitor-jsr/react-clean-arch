587c4e4e96a9c27554ee7a18489833f0c52310b8
heitor-jsr <heitorscalon@gmail.com>
2023-07-15 14:02:26 -0300

    chore: add testing library

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,6 @@ module.exports = {
     //sempre que o jest encotnrar um arquivo scss ele vai tentar renderizar o seu códig. ao testa-lo
     //para impedir isso, usamos o cod. abaixo, com a extensao do identity-obj-proxy, o que faz com que o jest
     //use a lib acima toda a vez que encotnrar um arquivo desses, convertendo o cod scss em um test double/dummy a ser testado pelo jest previnindo que ele tente testar os códigos scss, impedindo que esses arquivos interfiram no nosso teste
-    '\\scss$': 'identity-obj-proxy'
+    '\\.(scss|sass|css)$': 'identity-obj-proxy'
   }
 }
