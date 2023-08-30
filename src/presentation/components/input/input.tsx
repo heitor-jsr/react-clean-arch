bb9dd8c5279f432b68beac4e40a6f081529a69af
heitor-jsr <heitorscalon@gmail.com>
2023-08-30 19:16:00 -0300

    feat: ensure login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -15,11 +15,11 @@ const Input: React.FC<Props> = (props: Props) => {
   const error = state[`${props.name}Error`]
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
